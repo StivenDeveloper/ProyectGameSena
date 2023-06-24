bd10a238fffa97d7ccc551e8d747395283fe2f0a
StivenDeveloper <evertstiven619@gmail.com>
2023-06-23 22:39:20 -0500

    Se ambientó bajo la serie de pokemon y tiene una estructura de logica v1

--- js/game.js
+++ js/game.js
@@ -15,20 +15,21 @@ const botonAtaque = document.getElementById('boton-ataque');
 const botonDefensa= document.getElementById('boton-defensa');
 const botonSiguente = document.getElementById('siguente-round');
 const reinicar = document.getElementById('reiniciar-juego');
+const logoIncio = document.getElementById('logo-inicio');
 let num = 0;
 let personajes = [];
 let ataques = [];
 let defensas = [];
 let opcionPersonaje;
-let inputMario;
-let inputLuigi;
-let inputToad;
-let inputFuego;
-let inputAgua;
-let inputHacha;
-let inputCaAzul;
-let inputCaRojo;
-let inputCaAmarillo;
+let inputAsh;
+let inputBrock;
+let inputDawn;
+let inputElectrico1;
+let inputElectrico2;
+let inputRoca1;
+let inputRoca2;
+let inputAgua1;
+let inputAgua2;
 let personajeJugador;
 let personajeEnemigo;
 let imgLucha;
@@ -38,32 +39,32 @@ let ataqueJugador;
 let ataqueEnemigo;
 
 class Personaje{
-    constructor(nombre,foto,audio){
+    constructor(nombre,foto,fotoLucha){
         this.nombre = nombre;
         this.foto = foto;
-        this.audio= audio;
+        this.fotoLucha = fotoLucha;
     }
 }
 
-let mario = new Personaje("Mario", "assets/mario.png","song/mario.mp3");
-let luigi = new Personaje("Luigi","assets/luigi.png","song/luigi.mp3");
-let toad = new Personaje("Toad","assets/toad.png","song/toad.mp3");
+let ash = new Personaje("Ash", "assets/characters/ash1.png","assets/characters/ash2.png");
+let brock = new Personaje("Brock","assets/characters/Brock1.png","assets/characters/Brock2.png");
+let dawn = new Personaje("Dawn","assets/characters/dawn1.png","assets/characters/dawn2.png");
 
-let bowser= new Personaje("Bowser","assets/bowser.png","");
-let wario = new Personaje("Wario","assets/wario.png","");
-let waluigi = new Personaje("Waluigi","assets/waluigi.png","");
+let jessie= new Personaje("Jessie","assets/characters/Jessie1.png","assets/characters/Jessie2.png");
+let james= new Personaje("James","assets/characters/James1.png","assets/characters/James1.png");
+let helio = new Personaje("Helio","assets/characters/helio1.png","assets/characters/helio2.png");
 
-personajes.push(mario,luigi,toad);
+personajes.push(ash,brock,dawn);
 ataques.push(
-    {id:"agua", boton:""},
-    {id:"fuego",  boton:""},
-    {id:"hacha", boton:""},
+    {id:"Pikachu", imagen:""},
+    {id:"Golem",  imagen:""},
+    {id:"Empeleon", imagen:""},
 );
 
 defensas.push(
-    {id:"azul", boton: ""},
-    {id:"rojo", boton:""},
-    {id:"amarillo", boton:""},
+    {id:"Raichu", imagen: ""},
+    {id:"Aron", imagen:""},
+    {id:"Blastoise", imagen:""},
 );
 
 botonPersonajeJugador.style.display="none";
@@ -75,41 +76,38 @@ function inicioJuego(evento){
     evento.preventDefault;
     sectionIncio.style.display = "none";
     botonPersonajeJugador.style.display = "block"
-    ModificarFondo('assets/fondo2.jpg');
+    ModificarFondo('assets/font/seleccionarPesonaje.gif');
+    logoIncio.innerHTML = mostrarImagen('assets/font/logo.png','logo','logo')
     personajes.forEach((Personaje)=>{
         opcionPersonaje=`
         <input type="radio" name="personaje" id=${Personaje.nombre}>
         <label id=${Personaje.nombre} class="tarjeta-de-personajes" for=${Personaje.nombre}>
-            <p>${Personaje.nombre}</p>
-            <img src=${Personaje.foto} alt=${Personaje.nombre}>
+            <img src=${Personaje.foto} id=${Personaje.nombre} alt=${Personaje.nombre}>
         </label>
         `
         contenderTarjetas.innerHTML += opcionPersonaje;
-        inputMario = document.getElementById("Mario")
-        inputLuigi = document.getElementById("Luigi")
-        inputToad = document.getElementById("Toad")
+        inputAsh = document.getElementById("Ash")
+        inputBrock = document.getElementById("Brock")
+        inputDawn = document.getElementById("Dawn")
 
         
     })
 
-    reproducirAudioPersonaje(inputMario,mario.audio)
-    reproducirAudioPersonaje(inputLuigi,luigi.audio)
-    reproducirAudioPersonaje(inputToad,toad.audio)
     botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador)
     
 }
 
 function seleccionarPersonajeJugador(){
     sectionPersonaje.style.display= "none";
-    if(inputMario.checked){
-        personajesLuchaJugador.innerHTML = mostrarImagen(mario.foto,mario.nombre);
-        personajeJugador= mario.nombre;
-    }else if( inputLuigi.checked){
-        personajesLuchaJugador.innerHTML = mostrarImagen(luigi.foto,luigi.nombre);
-        personajeJugador= luigi.nombre;
-    }else if(inputToad.checked){
-        personajesLuchaJugador.innerHTML = mostrarImagen(toad.foto,toad.nombre);
-        personajeJugador= toad.nombre;
+    if(inputAsh.checked){
+        personajesLuchaJugador.innerHTML = mostrarImagen(ash.foto,ash.nombre,"imgAsh");
+        personajeJugador= ash.nombre;
+    }else if( inputBrock.checked){
+        personajesLuchaJugador.innerHTML = mostrarImagen(brock.foto,brock.nombre,"imgBrock");
+        personajeJugador= brock.nombre;
+    }else if(inputDawn.checked){
+        personajesLuchaJugador.innerHTML = mostrarImagen(dawn.foto,dawn.nombre,"imgDawn");
+        personajeJugador= dawn.nombre;
     }else{
         alert("¡Oye! seleccione a un personeje para la lucha")
         reiniciarJuego();
@@ -118,8 +116,6 @@ function seleccionarPersonajeJugador(){
     mostrarAtaquesDefensas(ataques,ataquesBotones);
     mostrarAtaquesDefensas(defensas,defensaBotones);
    
-    condenedorAtaque.style.display="none";
-    condenedorDefensa.style.display="none";
     num = random(1,2)
     botonesAtaqueAleatorio();
 }
@@ -163,21 +159,21 @@ function resultadoFinal(){
 }
 
 function ataqueLucha(){
-    inputAgua = document.getElementById('agua');
-    inputFuego = document.getElementById('fuego');
-    inputHacha = document.getElementById('hacha');
-    ataqueJugador = (inputAgua.checked)? "agua": (inputFuego.checked)? "fuego":"hacha";
-    ataqueEnemigo =  ataqueEnemigoAleatorio("azul","rojo","amarillo");
-    if(ataqueJugador=="agua" && ataqueEnemigo=="azul"|| ataqueJugador=="fuego" && ataqueEnemigo=="rojo"|| ataqueJugador=="hacha"&& ataqueEnemigo=="amarillo"){
+    inputElectrico1 = document.getElementById('Pikachu');
+    inputRoca1 = document.getElementById('Golem');
+    inputAgua1 = document.getElementById('Empeleon');
+    ataqueJugador = (inputElectrico1.checked)? "Pikachu": (inputRoca1.checked)? "Golem":"Empeleon";
+    ataqueEnemigo =  ataqueEnemigoAleatorio("Raichu","Aron","Blastoise");
+    if(ataqueJugador=="Pikachu" && ataqueEnemigo=="Raichu"|| ataqueJugador=="Golem" && ataqueEnemigo=="Aron"|| ataqueJugador=="Empeleon"&& ataqueEnemigo=="Blastoise"){
         vidasEnemigo -=10;
         mostrarMensajeAtaque(ataqueJugador,ataqueEnemigo,10)
-    }else if(ataqueJugador=="agua" && ataqueEnemigo=="rojo"|| ataqueJugador=="fuego" && ataqueEnemigo=="amarillo"|| ataqueJugador=="hacha"&& ataqueEnemigo=="azul"){
+    }else if(ataqueJugador=="Pikachu" && ataqueEnemigo=="Aron"|| ataqueJugador=="Golem" && ataqueEnemigo=="Blastoise"|| ataqueJugador=="Empeleon"&& ataqueEnemigo=="Raichu"){
         vidasEnemigo -=20;
         mostrarMensajeAtaque(ataqueJugador,ataqueEnemigo,20)
     }else{
         mostrarMensajeAtaque(ataqueJugador,ataqueEnemigo,0)
     }
-    if(inputAgua.checked||inputFuego.checked||inputHacha.checked){
+    if(inputElectrico1.checked||inputRoca1.checked||inputAgua1.checked){
         botonSiguente.style.display="block";
         botonAtaque.style.cursor="not-allowed"
         botonAtaque.disabled = true;
@@ -186,21 +182,21 @@ function ataqueLucha(){
 }
 
 function defensaLucha(){
-    inputCaAzul = document.getElementById('azul');
-    inputCaRojo = document.getElementById('rojo');
-    inputCaAmarillo = document.getElementById('amarillo');
-    ataqueJugador = (inputCaAzul.checked)? "azul": (inputCaRojo.checked)? "rojo":"amarillo";
-    ataqueEnemigo =  ataqueEnemigoAleatorio("agua","fuego","hacha");
-    if(ataqueJugador=="azul" && ataqueEnemigo=="agua"|| ataqueJugador=="rojo" && ataqueEnemigo=="fuego"|| ataqueJugador=="amarillo"&& ataqueEnemigo=="hacha"){
+    inputElectrico2 = document.getElementById('Raichu');
+    inputRoca2 = document.getElementById('Aron');
+    inputAgua2 = document.getElementById('Blastoise');
+    ataqueJugador = (inputElectrico2.checked)? "Raichu": (inputRoca2.checked)? "Aron":"Blastoise";
+    ataqueEnemigo =  ataqueEnemigoAleatorio("Pikachu","Golem","Empeleon");
+    if(ataqueJugador=="Raichu" && ataqueEnemigo=="Pikachu"|| ataqueJugador=="Aron" && ataqueEnemigo=="Golem"|| ataqueJugador=="Blastoise"&& ataqueEnemigo=="Empeleon"){
         vidasJugador -=10;
         mostrarMensajeDefensa(ataqueJugador,ataqueEnemigo,10)
-    }else if(ataqueJugador=="azul" && ataqueEnemigo=="hacha"|| ataqueJugador=="rojo" && ataqueEnemigo=="agua"|| ataqueJugador=="amarillo"&& ataqueEnemigo=="fuego"){
+    }else if(ataqueJugador=="Raichu" && ataqueEnemigo=="Empeleon"|| ataqueJugador=="Aron" && ataqueEnemigo=="Pikachu"|| ataqueJugador=="Blastoise"&& ataqueEnemigo=="Golem"){
         vidasJugador -=20;
         mostrarMensajeDefensa(ataqueJugador,ataqueEnemigo,20)
     }else{
         mostrarMensajeDefensa(ataqueJugador,ataqueEnemigo,0)
     }
-    if(inputCaAzul.checked|| inputCaRojo.checked|| inputCaAmarillo.checked){
+    if(inputElectrico2.checked|| inputRoca2.checked|| inputAgua2.checked){
         botonSiguente.style.display="block";
         botonDefensa.style.cursor="not-allowed"
         botonDefensa.disabled = true;
@@ -210,8 +206,8 @@ function defensaLucha(){
 
 function mostrarMensajeAtaque(ataqueJugador,ataqueEnemigo,vida){
     let mensaje = `
-      <p>${personajeJugador} ha atacado con ${ataqueJugador}, ${personajeEnemigo} se ha defendido con 
-      el caparazón ${ataqueEnemigo}, le has quitado ${vida} de vida.</p>
+      <p>${personajeJugador} ha atacado con el pokepon ${ataqueJugador}, ${personajeEnemigo} se ha defendido con 
+      el pokemon ${ataqueEnemigo}, le has quitado ${vida} de vida.</p>
     `
     mensajeResultado.innerHTML = mensaje;
 }
@@ -224,8 +220,8 @@ function mensajeFinal(msg){
 }
 function mostrarMensajeDefensa(ataqueJugador,ataqueEnemigo,vida){
     let mensaje = `
-      <p>${personajeEnemigo} ha atacado con ${ataqueEnemigo}, ${personajeJugador} se ha defendido con 
-      el caparazón ${ataqueJugador}, te han quitado ${vida} de vida.</p>
+      <p>${personajeEnemigo} ha atacado con el pokemon ${ataqueEnemigo}, ${personajeJugador} se ha defendido con 
+      el pokemon ${ataqueJugador}, te han quitado ${vida} de vida.</p>
     `
     mensajeResultado.innerHTML = mensaje;
 }
@@ -247,23 +243,23 @@ function enemigoAleatorio(){
     let num = random(1,3);
     switch(num){
         case 1:
-            personajesLuchaEnemigo.innerHTML=mostrarImagen(bowser.foto,bowser.nombre);
-            personajeEnemigo = bowser.nombre;
+            personajesLuchaEnemigo.innerHTML=mostrarImagen(jessie.foto,jessie.nombre,"imgJessie");
+            personajeEnemigo = jessie.nombre;
         break;
         case 2:
-            personajesLuchaEnemigo.innerHTML=mostrarImagen(wario.foto,wario.nombre);
-            personajeEnemigo = wario.nombre;
+            personajesLuchaEnemigo.innerHTML=mostrarImagen(james.foto,james.nombre,"imgJames");
+            personajeEnemigo = james.nombre;
         break;
         case 3:
-            personajesLuchaEnemigo.innerHTML=mostrarImagen(waluigi.foto,waluigi.nombre);
-            personajeEnemigo = waluigi.nombre;
+            personajesLuchaEnemigo.innerHTML=mostrarImagen(helio.foto,helio.nombre,"imgHelio");
+            personajeEnemigo = helio.nombre;
             
     }                   
 }
 
-function mostrarImagen(direccion,nombre){
+function mostrarImagen(direccion,nombre,id){
     let imagen = `
-    <img src=${direccion} alt=${nombre}>
+    <img src=${direccion} alt=${nombre} id=${id}>
     `
     return imagen;
 }
